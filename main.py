f48502f0439033f419c5f91028961e2d5108a359
ethanblum <ethan.t.blum@gmail.com>
2023-10-24 15:47:25 -0500

    Updated SQL Injection Page and added content. Need to add more pages.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, session
 from flask import render_template
 import bleach
+import sqlite3
 
 app = Flask(__name__, template_folder='templates' )
-app.secret_key = 'CSC440Secret!' #Don't know what to do with this
+app.secret_key = 'CSC440Secret!' #Don't know what to do with this it seems to be needed...
 
 @app.route('/')
 @app.route('/home')
@@ -24,6 +25,23 @@ def submit_form():
     submitted_message = session.pop('submitted_message', None)
     print(f"Name: {name}, Email: {email}, Message: {message}")
     session['submitted_message'] = 'Submitted successfully!'
+
+    try:
+        # Connect to the SQLite database
+        with sqlite3.connect('SSQL_DBMS.db') as conn:
+
+            cursor = conn.cursor()
+
+            cursor.execute("INSERT INTO contactUS (username, email, message) VALUES ('{}', '{}', '{}');".format(name, email, message))
+
+            cursor.execute('SELECT * FROM contactUS;')
+
+            print(cursor.fetchall())
+            # Commit the changes
+            conn.commit()
+
+    except sqlite3.Error as e:
+        print("SQLite error:", e)
 
     return render_template('homePage.html', submitted_message=submitted_message)
 
